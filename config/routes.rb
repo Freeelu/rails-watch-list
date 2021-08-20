cf85b39943621e77fc6b5817e7b52a675fbb593d
Freeelu <freeelu@gmail.com>
2021-08-20 11:08:36 +0200

    day 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/destroy'
+  get 'lists/create'
+  get 'lists/index'
+  get 'lists/new'
+  get 'lists/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: [ :create, :index, :new, :show ] do
+    resources :bookmarks, only: [ :new, :create ]
+
+  end
+  resources :bookmarks, only: [ :destroy ]
 end
